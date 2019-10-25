068b359e816d51857d1e4f8826a8801539b4f9b4
Sergi Castellsagué <s.castellsague@stuart.fr>
2019-10-25 17:58:35 +0200

    Change factor to 1 as any number higher cause an overflow

--- lib/src/manager.dart
+++ lib/src/manager.dart
@@ -553,7 +553,7 @@ class _Backoff {
   num _jitter;
   num attempts;
 
-  _Backoff({min = 100, max = 10000, jitter = 0, factor = 2})
+  _Backoff({min = 100, max = 10000, jitter = 0, factor = 1})
       : _ms = min,
         _max = max,
         _factor = factor {
